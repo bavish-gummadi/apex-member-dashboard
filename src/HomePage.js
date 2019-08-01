4bc9127c3e3bcc1f137597b21ac83ad9d4fd7091
Robert <xuro@umich.edu>
2019-07-31 21:13:42 -0400

    Added css shell for HeaderBar and SearchBar

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,6 +3,39 @@ import logo from './images/logo.png';
 import { withFirebase } from './Firebase';
 import * as ROUTES from './constants/routes';
 
+function Header(props) {
+    return (
+    <div>    
+      <header className="header">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-12">
+              <img className="logo" src={logo} />
+              <ul class="site-nav">
+                <li class="nav-item"><a href="" class="nav-link">PEOPLE</a> </li>
+                <li class="nav-item"><a href="" class="nav-link">PROJECTS</a> </li>
+                <li class="nav-item"><a href="" class="nav-link">RESOURCES</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>    
+    );
+  }
+
+function SearchBar(props) {
+    return (
+   <div class="search-box">
+                        <div class="inner">
+                            <form action="{% url 'parks:search-results' %}" method="get" accept-charset="utf-8">
+                                <input type="text" placeholder="Search" name="q" /><button type="submit">SEARCH</button>
+                            </form>
+                        </div>
+                </div>
+    )
+}
+ 
 class HomePage extends Component {
   constructor(props) {
     super(props);
@@ -19,13 +52,17 @@ class HomePage extends Component {
   render(props) {
     return (
       <div>
+        <div className="container-side">
+            <Header />
+            <SearchBar />
+        </div>
         <header className="App-header">
           {this.props.firebase.user ? (
             <>
               <h2>
                 Hi {this.props.firebase.userData.name.first}
               </h2>
-              <img src={this.props.firebase.userData.photoURL} alt="userPhoto"/>
+              <img className="profile" src={this.props.firebase.userData.photoURL} alt="userPhoto"/>
             </>
           ) : (
             <h2>
@@ -36,7 +73,6 @@ class HomePage extends Component {
           <h2>
             Apex Member Dashboard
           </h2>
-          <img src={logo} className="App-logo" alt="logo" />
           <button onClick={this.signOut}>HERE SIGN OUT</button>
           <button onClick={this.logCredentials}>HERE TO LOG CREDS</button>
 
